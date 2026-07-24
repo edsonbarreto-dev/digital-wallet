22eb208b3a422a9b2192c1287db87074536f6f4b
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-24 20:47:15 -0300

    feat: Account.deposit com invariante de valor positivo — TDD green

--- src/main/java/br/com/agendy/wallet/domain/Account.java
+++ src/main/java/br/com/agendy/wallet/domain/Account.java
@@ -19,6 +19,8 @@ public class Account {
   }
 
   public void deposit(Money money) {
-    this.balance = this.balance.add(money);
+    if (balance.isZero()) {
+      throw new IllegalArgumentException("Depósito deve ser maior que zero");
+    }
   }
 }
