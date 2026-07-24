01d75fbe2254481c25145a1d9efd603e6b20a085
Edson Silva Barreto <edson.barreto@outlook.com>
2026-07-24 20:20:52 -0300

    feat: Account.deposit — deposita valor na conta — TDD red

--- src/test/java/br/com/agendy/wallet/domain/AccountTest.java
+++ src/test/java/br/com/agendy/wallet/domain/AccountTest.java
@@ -13,4 +13,11 @@ public class AccountTest {
     Account conta = Account.open();
     assertThat(conta.balance().amount()).isEqualByComparingTo(BigDecimal.ZERO);
   }
+
+  @Test
+  void deposito_aumenta_o_saldo() {
+    Account conta = Account.open();
+    conta.deposit(Money.of(new BigDecimal("10.00")));
+    assertThat(conta.balance()).isEqualTo(Money.of(new BigDecimal("10.00")));
+  }
 }
